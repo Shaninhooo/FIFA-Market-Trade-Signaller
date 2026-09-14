0ce91305e1babd376c186b81f0e94fb2fadca212
shaninho <shanelee1007@gmail.com>
2026-09-14 15:41:46 +0930

    DB variable names updated

--- src/db_utils.py
+++ src/db_utils.py
@@ -9,10 +9,10 @@ load_dotenv()
 
 def get_connection():
     return pymysql.connect(
-        host = os.getenv("DB_HOST"),
-        user = os.getenv("DB_USER"),
-        password = os.getenv("DB_PASSWORD"),
-        database = os.getenv("DB_NAME"),
+        host = os.getenv("MYSQLHOST"),
+        user = os.getenv("MYSQLUSER"),
+        password = os.getenv("MYSQLPASSWORD"),
+        database = os.getenv("MYSQLDATABASE"),
         charset="utf8mb4",
         cursorclass=pymysql.cursors.DictCursor
     )
