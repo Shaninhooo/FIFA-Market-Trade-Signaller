1fc8bb3e78f40366220f3840c1da301c8cc530a6
shaninho <shanelee1007@gmail.com>
2026-09-17 06:41:01 +0930

    refactor(scraper): limit card versions in main_scrape to "gold_rare"

--- src/scraper.py
+++ src/scraper.py
@@ -573,7 +573,10 @@ async def get_sales(sales_href, session):
 async def main_scrape():
 
     # Get all card versions
-    versions = ["gold_rare", "base_icon", "team_of_the_week"]
+    versions = ["gold_rare"]
     for version in versions:
-            collect_all_hrefs(version)  # synchronous
+            # collect_all_hrefs(version)  # synchronous
             await scrape_players(version)  # async
+
+
+# , "base_icon", "team_of_the_week"
